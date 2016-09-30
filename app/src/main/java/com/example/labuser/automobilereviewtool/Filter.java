747fd46b64ecb76121eae056e1c0efa493c7d62b
Nsoha <nicholas.soha@maine.edu>
2016-09-30 01:52:23 -0400

    1

--- app/src/main/java/com/example/labuser/automobilereviewtool/Filter.java
+++ app/src/main/java/com/example/labuser/automobilereviewtool/Filter.java
@@ -1,9 +1,11 @@
 package com.example.labuser.automobilereviewtool;
 
+import android.app.SearchManager;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -22,44 +24,71 @@ public class Filter extends AppCompatActivity {
     ReviewDBHelper mDbHelper;
     SQLiteDatabase db;
 
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter);
 
         mDbHelper = new ReviewDBHelper(this);
         db = mDbHelper.getReadableDatabase();
+
     }
 
-        protected void filterRecord(View v){
+            public Cursor filter (String query, String[] columns) {
+                String selection = ((TextView) findViewById(R.id.txtFilter)).getText().toString() + " MATCH ?";
+                String[] selectionArgs = new String[] {query+"*"};
 
-            String f = ((TextView) findViewById(R.id.btnSearch)).getText().toString();
+                return query(selection, selectionArgs, columns);
+            }
 
-        Cursor c = db.query(true, TABLE_NAME, new String[] {COLUMN_NAME_REVIEWER,
-                        COLUMN_NAME_TITLE }, COLUMN_NAME_RATING + " LIKE ?",
-                new String[] { f+"%" }, null, null, null,
-                null);
+            public Cursor query (String selection, String[] selectionArgs, String[] columns) {
+                SQLiteQueryBuilder builder = new SQLiteQueryBuilder();
+                builder.setTables(TABLE_NAME);
 
+                Cursor cursor = builder.query(mDbHelper.getReadableDatabase(),
+                        columns, selection, selectionArgs, null, null, null);
 
-
-        ReviewCursorAdapter adapter = new ReviewCursorAdapter(this,c);
-
-        ListView listview = (ListView) findViewById(R.id.listView);
-
-        listview.setAdapter(adapter);
-
-        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                Intent intent = new Intent(view.getContext(), DetailView.class);
-
-                intent.putExtra("position",position);
-
-                startActivity(intent);
+                if (cursor == null) {
+                    return null;
+                } else if (!cursor.moveToFirst()) {
+                    cursor.close();
+                    return null;
+                }
+                return cursor;
             }
 
 
-        });
-    }
-}
+                public void handleIntent(Intent intent) {
+
+                    if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+                        String query = intent.getStringExtra(SearchManager.QUERY);
+                        Cursor c = filter(query, null);
+                        //process Cursor and display results
+
+
+                        ReviewCursorAdapter adapter = new ReviewCursorAdapter(this, c);
+
+                        ListView listview = (ListView) findViewById(R.id.listView);
+
+                        listview.setAdapter(adapter);
+
+
+                        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                                Intent intent = new Intent(view.getContext(), DetailView.class);
+
+                                intent.putExtra("position", position);
+
+                                startActivity(intent);
+                            }
+
+
+                        });
+                    }
+                }
+            }
+
+
